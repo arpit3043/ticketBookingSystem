2f2c772768600141708d69731543027bd13a9512
arpit3043 <arpit3043@gmail.com>
2023-02-06 00:25:58 +0530

    Rheo.AI Assignment

---
+++ rheo-AI-assignment/src/components/Layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar"
+const Layout = ({ children }: any) => {
+  return (
+    <div className="content">
+      <Navbar />
+      { children }
+    </div>
+  );
+}
+export default Layout;
---
+++ rheo-AI-assignment/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link href="/">
+        <div className="logo">
+          {/*<Image src="/LCL Logo.png" alt="site logo" width={250} height={100} />*/}
+          <span className="logo-text">Ticket Booking System</span>
+        </div>
+      </Link>
+      <Link href="/"><a>Home</a></Link>
+    </nav>
+);
+}
+ 
+export default Navbar;
